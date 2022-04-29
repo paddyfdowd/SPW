2835d526ed998940de13973ad43f8632532f5f02
Vladyslav 🦁 <vladyslav@pc-72-163.customer.ask4.lan>
2022-04-29 22:24:35 +0100

    get back the /path/ dir

---
+++ assets/js/signup.js
@@ -0,0 +1,44 @@
+function phpSignup() {
+
+  var uname = document.getElementById("signup_1").value;
+  var name = document.getElementById("signup_2").value;
+  var address = document.getElementById("signup_3").value;
+  var pword = document.getElementById("signup_4").value;
+  var passSymbols = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+
+  if (!pword.match(passSymbols)){  
+    alert("Password should be a minimum of 6 symbols and contain one small and one big alphabet symbol!");
+    return false;
+  }
+
+  if (uname && name && address && pword !== "") {
+    $.ajax({
+        type: "POST",
+        url: "/dashboard/SPW/SPW/SPW3/hyperspace/php/hash.php",
+        data: {
+          "uname": uname,
+          "pword": pword,
+          "name": name,
+          "address": address
+        }
+      })
+      .done(function (data, textStatus, jqXHR) {
+        alert("Success: " + data);
+        if (data == "userexists") {
+          alert("Username unavailable")
+        } else {
+          window.location.href = "http://localhost/dashboard/SPW/SPW/SPW3/hyperspace/profile.html"
+        }
+      })
+      .fail(function (jqXHR, textStatus, errorThrown) {
+        alert("Error" + errorThrown + textStatus);
+      })
+      .always(function (jqXHROrData, textStatus, jqXHROrErrorThrown) {
+        //alert("complete"); 
+      });
+  } else {
+    alert("Please fill in all fields");
+  }
+}
+
+document.getElementById("signupbutton").addEventListener("click", phpSignup);
